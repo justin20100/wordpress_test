de7e8dd4a6120bf44e2d66246fb980ce656844ae
justinvincent <justin.vincent@student.hepl.be>
2022-03-16 10:47:21 +0100

    addded custom contact form"

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -25,6 +25,24 @@ register_post_type('trip', [
     'rewrite' => ['slug' => 'voyages'],
 ]);
 
+// enregistrer un custom post-type pour les messages de contact
+register_post_type('message', [
+    'label' => 'Message de contact',
+    'labels' => [
+        'name' => 'Messages de contact',
+        'singular_name' => 'Messages de contact',
+    ],
+    'description' => 'Les messages envoye par le formulaire de contact',
+    'public' => false,
+    'show_ui' => true,
+    'menu_position' => 15,
+    'menu_icon' => 'dashicons-buddicons-pm',
+    'capabilities' => [
+        'create_posts'=> false,
+        'read_post'=>true,
+        'read_post_private'=>true,
+    ],
+]);
 // Récupérer les trips via une requête Wordpress
 function dw_get_trips($count = 20)
 {
@@ -83,4 +101,49 @@ function dw_get_menu_items($location)
 
     // Retourner un tableau d'éléments du menu formatés
     return $items;
+}
+
+// gerer l'envoi de formulaire personnalisé
+
+add_action( 'admin_post_submit_contact_form', 'dw_handle_submit_contact_form' );
+
+function dw_handle_submit_contact_form ()
+{
+    $nonce = $_POST['_wpnonce'];
+    if (wp_verify_nonce($nonce,'nonce_submit_contact')){
+        $firstname = sanitize_text_field($_POST['firstname']);
+        $lastname = sanitize_text_field($_POST['firstname']);
+        $email = sanitize_email($_POST['email']);
+        $phone = sanitize_text_field($_POST['phone']);
+        $message = sanitize_text_field($_POST['message']);
+        $rules = sanitize_text_field($_POST['rules'] ?? '');
+
+        if ($firstname && $lastname && $email && $message){
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                if ($rules === '1'){
+                    $id = wp_insert_post([
+                        'post_title'=>'Messages de ' . $firstname . ' ' . $lastname,
+                        'post_type'=>'message',
+                        'post_content'=> $message,
+                        'post_status'=> 'publish'
+                    ]);
+                    //generer un eail contenant l'url vers le post en question
+                    $feedback = 'Bonjur, vous avez un nouveau messages.';
+                    $feedback .= 'y acceder : ' . get_edit_post_link($id);
+                    //envoyer l'email a l'admin
+                    wp_mail(get_bloginfo('admin_email'),'Nouveau message', );
+                }else{
+                    // to do : retourner une valeur de validation
+                }
+            }else{
+                // to do email non valide
+            }
+        }else {
+            // to do retourner une valeur de validation 'champs requis
+        }
+    }
+    // to do :  retourner un message d'erreur 'not authorizeed'
+
+
+
 }
--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -6,9 +6,40 @@
         <div class="contact__content">
             <?php the_content(); ?>
         </div>
-        <div class="contact__form">
-            <?= apply_filters('the_content', '[contact-form-7 id="35" title="Formulaire de contact"]'); ?>
-        </div>
+        <form action="<?= get_home_url(); ?>/wp-admin/admin-post.php"  method="POST" class="contact__form">
+            <div class="form__field">
+                <labe for="firstname" class="form__label">Votre prénom</labe>
+                <input type="text" name="firstname" id="firstname" class="form__input">
+            </div>
+            <div class="form__field">
+                <labe for="lastname" class="form__label">Votre nom</labe>
+                <input type="text" name="lastname" id="lastname" class="form__input">
+            </div>
+            <div class="form__field">
+                <labe for="email" class="form__label">Votre mail</labe>
+                <input type="email" name="email" id="email" class="form__input">
+            </div>
+            <div class="form__field">
+                <labe for="phone" class="form__label">Votre numero</labe>
+                <input type="tel" name="phone" id="phone" class="form__input">
+            </div>
+            <div class="form__field">
+                <labe for="message" class="form__label">Votre message</labe>
+                <textarea name="message" id="message" cols="30" rows="10" class="form__input"></textarea>
+            </div>
+
+            <div class="form__field">
+                <label for="rules" class="form__label">
+                    <input type="checkbox" name="rules" id="rules">
+                    <span class="form__checklabel">j'accepte les <a href="">conditions générales d'utilisation</a></span>
+                </label>
+            </div>
+            <div class="form__actions">
+                <?php wp_nonce_field('nonce_contact_form') ?>
+                <input type="hidden" name="action" value="submit_contact_form">
+                <button class="form__button" type="submit">Envoyer</button>
+            </div>
+        </form>
     </main>
     <?php endwhile; endif; ?>
 <?php get_footer(); ?>
